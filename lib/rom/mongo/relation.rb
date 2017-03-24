cfb2d6ebd5c2ea23b6dd556e111b999b216d395e
Nikita Shilnikov <fg@flashgordon.ru>
2017-03-24 15:00:37 +0300

    Add Relation#by_pk

--- lib/rom/mongo/relation.rb
+++ lib/rom/mongo/relation.rb
@@ -8,6 +8,18 @@ module ROM
       use :key_inference
 
       forward :insert, :find, :only, :without, :skip, :limit, :where
+
+      # @!method by_pk(id)
+      #   Return a relation restricted by _id
+      #
+      #   @param id [BSON::ObjectId] Document's PK value
+      #
+      #   @return [Mongo::Relation]
+      #
+      #   @api public
+      def by_pk(id)
+        find(_id: id)
+      end
     end
   end
 end
---
+++ spec/unit/relation_spec.rb
@@ -0,0 +1,14 @@
+RSpec.describe ROM::Mongo::Relation do
+  include_context 'users'
+
+  describe '#by_pk' do
+    it 'fetches a document by _id' do
+      expect(users.by_pk(jane_id).one!).
+        to eql(
+             '_id' => jane_id,
+             'name' => 'Jane',
+             'email' => 'jane@doe.org'
+           )
+    end
+  end
+end
